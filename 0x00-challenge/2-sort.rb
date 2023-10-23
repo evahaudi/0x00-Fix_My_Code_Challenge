90f0e0e7b5e8c056952b9e2dbb2375110638af77
Everlyne akinyi <evahaudi2354@gmail.com>
2023-10-23 11:57:17 +0000

    updated sort

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -6,26 +6,13 @@
 
 result = []
 ARGV.each do |arg|
-    # skip if not integer
-    
-
     # convert to integer
     i_arg = arg.to_i
-    
-    # insert result at the right position
-    is_inserted = false
-    i = 0
-    l = result.size
-    while !is_inserted && i < l do
-        if result[i] < i_arg
-            i += 1
-        else
-            result.insert(i - 1, i_arg)
-            is_inserted = true
-            break
-        end
+    if i_arg.to_s == arg && i_arg >= 0
+      result << i_arg
     end
-    result << i_arg if !is_inserted
 end
 
-puts result
+result.sort!.each { |num| puts num }
+    
+
