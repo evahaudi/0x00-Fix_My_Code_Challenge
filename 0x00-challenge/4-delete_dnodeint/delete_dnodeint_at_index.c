17170f5053875643d5529851da4f07230c1343a0
Everlyne akinyi <evahaudi2354@gmail.com>
2023-10-23 12:37:18 +0000

    Revert "updated poiter"
    This reverts commit 32a90bfa7b0ed344685d0e3102b4a6074bc61e16.

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -41,5 +41,13 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			tmp->prev = NULL;
 		}
 	}
-
+	else
+	{
+		(*head)->prev->prev = (*head)->prev;
+		free(*head);
+		if ((*head)->next)
+			(*head)->next->prev = (*head)->prev;
+		*head = saved_head;
+	}
+	return (1);
 }
